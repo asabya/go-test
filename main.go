b713fd00304a41192392fa4cb0f4b1d1ade51739
asabya <asabya@gmail.com>
2022-09-14 08:29:12 +0530

    download fairos-dfs separately

--- main.go
+++ main.go
@@ -2,16 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/helloworld", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w, "Hello, World!")
-	})
-	fmt.Printf("Server running (port=80), route: http://localhost:80/helloworld\n")
-	if err := http.ListenAndServe(":8081", nil); err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println("hello")
 }
